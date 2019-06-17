21523106e48c4703e87c3b01f0bcc588e5f003b6
Kris de Rycke <derycke.kris@gmail.com>
2019-06-17 10:12:13 +0200

    declare 4 variables

---
+++ index.js
@@ -0,0 +1,4 @@
+let rock = document.getElementById("Rock");
+let paper = document.getElementById("Paper");
+let scissor = document.getElementById("Scissors");
+let random = document.getElementById("Random");
