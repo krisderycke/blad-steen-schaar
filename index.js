27590ee7fd6e70949359b4e3a1b31eb3dfa6b536
Kris de Rycke <derycke.kris@gmail.com>
2019-06-18 10:23:04 +0200

    change randomize functions so that array uses classes i.s.o src atrributes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Pc = document.getElementById("Pc");
 Rock.addEventListener("click", rock);
 Paper.addEventListener("click", paper);
 Scissor.addEventListener("click", scissor);
-Random.addEventListener("click", randomUser);
+// Random.addEventListener("click", randomUser);
 Fight.addEventListener("click", fight);
 Reset.addEventListener("click", reset);
 
@@ -25,101 +25,100 @@ function rock() {
   console.log("rock");
 }
 function paper() {
-  document.getElementById("User").src = "/Assets/images/paper.jpg";
+  User.className = "paper";
   console.log("paper");
 }
 
 function scissor() {
-  document.getElementById("User").src = "/Assets/images/scissors.png";
+  User.className = "scissors";
   console.log("scicc");
 }
 
-function randomUser() {
-  var randomImages = [
-    "/Assets/images/rock.png",
-    "/Assets/images/paper.jpg",
-    "/Assets/images/scissors.png"
-  ];
+// function randomUser() {
+//   var randomImages = [
+//     "/Assets/images/rock.png",
+//     "/Assets/images/paper.jpg",
+//     "/Assets/images/scissors.png"
+//   ];
 
-  var rand = randomImages[Math.floor(Math.random() * randomImages.length)];
-  document.getElementById("User").src = rand;
-  console.log("random");
-}
+//   var rand = randomImages[Math.floor(Math.random() * randomImages.length)];
+//   document.getElementById("User").src = rand;
+//   console.log("random");
 
 //making "fight button" pick a random image
 
 function fight() {
-  var randomImagesPc = [
-    "/Assets/images/rock.png",
-    "/Assets/images/paper.jpg",
-    "/Assets/images/scissors.png"
-  ];
+  var randomClassesPc = ["rock", "paper", "scissors"];
 
   var randPc =
-    randomImagesPc[Math.floor(Math.random() * randomImagesPc.length)];
-  document.getElementById("Pc").src = randPc;
+    randomClassesPc[Math.floor(Math.random() * randomClassesPc.length)];
+  Pc.className = randPc;
   console.log("randomPc");
 
-  var span = document.getElementById("span");
-  if (
-    User.src == "/Assets/images/rock.png" &&
-    Pc.src == "/Assets/images/rock.png"
-  ) {
-    span.innerHTML = "It's a tie, reset and start again";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/paper.jpg" &&
-    Pc.src == "/Assets/images/rock.png"
-  ) {
-    span.innerHTML = "You win!";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/scissors.png" &&
-    Pc.src == "/Assets/images/rock.png"
-  ) {
-    span.innerHTML = "Auwtch, You loose";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/rock.png" &&
-    Pc.src == "/Assets/images/paper.jpg"
-  ) {
-    span.innerHTML = " Aha, you shredded the opponent!";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/paper.jpg" &&
-    Pc.src == "/Assets/images/paper.jpg"
-  ) {
-    span.innerHTML = "Tie break. please reset";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/scissors.png" &&
-    Pc.src == "/Assets/images/paper.jpg"
-  ) {
-    span.innerHTML = " The enemy has defeated you";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/rock.png" &&
-    Pc.src == "/Assets/images/scissors.png"
-  ) {
-    span.innerHTML = "You crushed the foo";
-    console.log("result");
-  } else if (
-    User.src == "/Assets/images/paper.jpg" &&
-    Pc.src == "/Assets/images/scissors.png"
-  ) {
-    span.innerHTML = "The enemy has cut you in pieces, fight again!";
-    console.log("result");
-  } else
-    User.src == "./Assets/images/scissors.png" &&
-      Pc.src == "./Assets/images/scissors.png";
 
-  span.innerHTML = " reset the fight, one cannot win from the same element";
-  console.log("result");
-  console.log(User.src);
-  console.log(Pc.src);
-  console.log("false", User.src === "/Assets/images/scissors.png");
-  console.log(
-    "somtimes true",
-    Pc.src === "http://127.0.0.1:5500/Assets/images/scissors.png"
-  );
-}
+
+
+
+
+  
+
+//   var span = document.getElementById("span");
+//   if (User.className === "rock" && Pc.className === "rock") {
+//     span.innerHTML = "It's a tie, reset and start again";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/paper.jpg" &&
+//     Pc.src == "/Assets/images/rock.png"
+//   ) {
+//     span.innerHTML = "You win!";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/scissors.png" &&
+//     Pc.src == "/Assets/images/rock.png"
+//   ) {
+//     span.innerHTML = "Auwtch, You loose";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/rock.png" &&
+//     Pc.src == "/Assets/images/paper.jpg"
+//   ) {
+//     span.innerHTML = " Aha, you shredded the opponent!";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/paper.jpg" &&
+//     Pc.src == "/Assets/images/paper.jpg"
+//   ) {
+//     span.innerHTML = "Tie break. please reset";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/scissors.png" &&
+//     Pc.src == "/Assets/images/paper.jpg"
+//   ) {
+//     span.innerHTML = " The enemy has defeated you";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/rock.png" &&
+//     Pc.src == "/Assets/images/scissors.png"
+//   ) {
+//     span.innerHTML = "You crushed the foo";
+//     console.log("result");
+//   } else if (
+//     User.src == "/Assets/images/paper.jpg" &&
+//     Pc.src == "/Assets/images/scissors.png"
+//   ) {
+//     span.innerHTML = "The enemy has cut you in pieces, fight again!";
+//     console.log("result");
+//   } else
+//     User.src == "./Assets/images/scissors.png" &&
+//       Pc.src == "./Assets/images/scissors.png";
+
+//   span.innerHTML = " reset the fight, one cannot win from the same element";
+//   console.log("result");
+//   console.log(User.src);
+//   console.log(Pc.src);
+//   console.log("false", User.src === "/Assets/images/scissors.png");
+//   console.log(
+//     "somtimes true",
+//     Pc.src === "http://127.0.0.1:5500/Assets/images/scissors.png"
+//   );
+// }
